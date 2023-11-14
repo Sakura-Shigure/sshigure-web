c0761737df70d782ff99bec8e170573ac84383f6
Tony <admin@syntony666.com>
2023-11-15 05:04:24 +0800

    remove footer link

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,15 +38,8 @@ export default function RootLayout({
             <footer
               className={`w-full flex items-center justify-center py-3 ${fontSans.className}`}
             >
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">Sakura Shigure</p>
-              </Link>
+              <span className="text-default-600">Powered by</span>
+              <p className="text-primary">Sakura Shigure</p>
             </footer>
           </div>
         </Providers>
